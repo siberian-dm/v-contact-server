215ad221853fb690afcb823daf1cf5a863c82435
d.vorobjev <d.vorobjev@zordoc.uz>
2023-07-03 23:41:11 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
